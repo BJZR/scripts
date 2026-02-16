4f9b1d43cd6967a8bab2cf2ef6cd8f17d8664482
BJZR <132262356+BJZR@users.noreply.github.com>
2026-02-16 13:56:41 -0500

    Update pipewire.sh

--- voidlinux/pipewire.sh
+++ voidlinux/pipewire.sh
@@ -14,8 +14,8 @@ echo -e "${RESET}"
 
 # Paso 1: Instalar paquetes
 echo -e "📦 ${AZUL}Instalando paquetes necesarios...${RESET}"
-sudo xbps-install -y pipewire wireplumber blueman libspa-bluetooth pavucontrol
-
+sudo xbps-install -y pipewire wireplumber blueman libspa-bluetooth pavucontrol obexd
+# obexd : para inalambricos auriculares, pavu..: para gestiornar la salidas o entrada., wirelumper: dependencia de pipe.. blueman: el backend, libspa..: dependencia de blue. 
 # Paso 2: Agregar usuario al grupo bluetooth
 echo -e "👤 ${AZUL}Agregando el usuario '$USER' al grupo bluetooth...${RESET}"
 sudo usermod -aG bluetooth "$USER"
@@ -23,6 +23,10 @@ sudo usermod -aG bluetooth "$USER"
 # Paso 3: Activar el servicio bluetooth
 echo -e "👤 ${AZUL}Agregando el servicio bluetooth...${RESET}"
 sudo ln -s /etc/sv/bluetoothd /var/service
+
+# Paso 3: Activar el servicio obexd
+echo -e "👤 ${AZUL}Agregando el servicio obexd...${RESET}"
+sudo ln -s /etc/sv/obexd /var/service
 
 # Mensaje final
 echo -e "\n${VERDE}✅ Instalación y configuración completada.${RESET}"
